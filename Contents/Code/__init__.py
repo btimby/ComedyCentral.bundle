1e6dd60a8cfc6b68d98095c2887e07f926b661d3
Mike <mike.d.meier@gmail.com>
2012-10-14 11:59:25 -0600

    update to grab full episodes instead of just clips

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -9,8 +9,6 @@ SHOW_EXCLUSIONS = ["The Daily Show With Jon Stewart", "The Colbert Report", "Sou
 
 ####################################################################################################
 def Start():
-
-	Plugin.AddPrefixHandler("/video/comedycentral", MainMenu, "Comedy Central", ICON, ART)
 	ObjectContainer.art = R(ART)
 	DirectoryObject.thumb = R(ICON)
 	ObjectContainer.title1 = "Comedy Central"
@@ -19,6 +17,7 @@ def Start():
 	HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:13.0) Gecko/20100101 Firefox/13.0.1'
 
 ####################################################################################################
+@handler("/video/comedycentral", "Comedy Central", ICON, ART)
 def MainMenu():
 
 	oc = ObjectContainer()
@@ -39,6 +38,7 @@ def MainMenu():
 	return oc
 
 ####################################################################################################
+@route("/video/comedycentral/episodes")
 def Episodes(show_url, title):
 
 	oc = ObjectContainer(title2=title)
@@ -48,22 +48,47 @@ def Episodes(show_url, title):
 
 	show_page = HTML.ElementFromURL(show_url)
 	try:
-		id = show_page.xpath('//div[@id="video_player_box"]')[0].get('data-mgid')
+		#id = show_page.xpath('//div[@id="video_player_box"]')[0].get('data-mgid')
+		episodes = show_page.xpath('//div[@itemtype="http://schema.org/TVEpisode"]')[1:]
+		if len(episodes) == 0:
+			raise error
 	except:
-		episodes_url = show_page.xpath('//a[@class="episodes"]')[0].get('href')
+		try:
+			episodes_url = show_page.xpath('//a[@class="episodes"]')[0].get('href')
+		except:
+			episodes_url = show_url + 'full-episodes/'
 		show_page = HTML.ElementFromURL(episodes_url)
-		id = show_page.xpath('//div[@id="video_player_box"]')[0].get('data-mgid')
-	mrss_feed = XML.ElementFromURL(MRSS_PATH % id)
+		episodes = show_page.xpath('//div[@itemtype="http://schema.org/TVEpisode"]')[1:]
+		if len(episodes) == 0:
+			''' one last try to grab at least one episode '''
+			try:
+				HTTP.Request(show_url + 'full-episodes/', follow_redirects = False).headers
+			except Ex.RedirectError, e:
+				try:
+					episode_page = e.location
+					oc.add(URLService.MetadataObjectForURL(episode_page))
+				except: pass
+			
+		
+	
+	for episode in episodes:
+		url = episode.xpath('.//meta[@itemprop="url"]')[0].get('content')
+		title = episode.xpath('.//meta[@itemprop="name"]')[0].get('content')
+		thumb = episode.xpath('.//meta[@itemprop="image"]')[0].get('content')
+		summary = episode.xpath('.//meta[@itemprop="description"]')[0].get('content')
+		date = episode.xpath('.//meta[@itemprop="datePublished"]')[0].get('content')
+		date = Datetime.ParseDate(date).date()
+	#mrss_feed = XML.ElementFromURL(MRSS_PATH % id)
 
-	for item in mrss_feed.xpath('//item'):
-		url = item.xpath('./link')[0].text
-		title = item.xpath('./title')[0].text
-		summary = item.xpath('./description')[0].text
-		date = Datetime.ParseDate(item.xpath('./pubDate')[0].text).date()
-		thumb = item.xpath('.//media:thumbnail', namespaces=MRSS_NS)[0].get('url')
-		duration = int(float(item.xpath('.//media:content', namespaces=MRSS_NS)[0].get('duration'))*1000)
+	#for item in mrss_feed.xpath('//item'):
+	#	url = item.xpath('./link')[0].text
+	#	title = item.xpath('./title')[0].text
+	#	summary = item.xpath('./description')[0].text
+	#	date = Datetime.ParseDate(item.xpath('./pubDate')[0].text).date()
+	#	thumb = item.xpath('.//media:thumbnail', namespaces=MRSS_NS)[0].get('url')
+	#	duration = int(float(item.xpath('.//media:content', namespaces=MRSS_NS)[0].get('duration'))*1000)
 
-		oc.add(VideoClipObject(url=url, title=title, summary=summary, originally_available_at=date, duration=duration,
+		oc.add(VideoClipObject(url=url, title=title, summary=summary, originally_available_at=date,
 			thumb=Resource.ContentsOfURLWithFallback(url=thumb, fallback=ICON)))
 
 	if len(oc) == 0:
