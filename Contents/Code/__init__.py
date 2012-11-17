8d358bf61f66c96d72d7213e4c50113e088dd7ad
sander1 <sander@plexapp.com>
2012-11-17 20:46:12 +0100

    Optimize images; remove some commented out code.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -9,6 +9,7 @@ SHOW_EXCLUSIONS = ["The Daily Show With Jon Stewart", "The Colbert Report", "Sou
 
 ####################################################################################################
 def Start():
+
 	ObjectContainer.art = R(ART)
 	DirectoryObject.thumb = R(ICON)
 	ObjectContainer.title1 = "Comedy Central"
@@ -48,29 +49,29 @@ def Episodes(show_url, title):
 
 	show_page = HTML.ElementFromURL(show_url)
 	try:
-		#id = show_page.xpath('//div[@id="video_player_box"]')[0].get('data-mgid')
 		episodes = show_page.xpath('//div[@itemtype="http://schema.org/TVEpisode"]')[1:]
+
 		if len(episodes) == 0:
-			raise error
+			return ObjectContainer(header="Error", message="This category does not contain any video.")
 	except:
 		try:
 			episodes_url = show_page.xpath('//a[@class="episodes"]')[0].get('href')
 		except:
 			episodes_url = show_url + 'full-episodes/'
+
 		show_page = HTML.ElementFromURL(episodes_url)
 		episodes = show_page.xpath('//div[@itemtype="http://schema.org/TVEpisode"]')[1:]
+
 		if len(episodes) == 0:
 			''' one last try to grab at least one episode '''
 			try:
-				HTTP.Request(show_url + 'full-episodes/', follow_redirects = False).headers
+				HTTP.Request(show_url + 'full-episodes/', follow_redirects=False).headers
 			except Ex.RedirectError, e:
 				try:
 					episode_page = e.location
 					oc.add(URLService.MetadataObjectForURL(episode_page))
 				except: pass
-			
-		
-	
+
 	for episode in episodes:
 		url = episode.xpath('.//meta[@itemprop="url"]')[0].get('content')
 		title = episode.xpath('.//meta[@itemprop="name"]')[0].get('content')
@@ -78,15 +79,6 @@ def Episodes(show_url, title):
 		summary = episode.xpath('.//meta[@itemprop="description"]')[0].get('content')
 		date = episode.xpath('.//meta[@itemprop="datePublished"]')[0].get('content')
 		date = Datetime.ParseDate(date).date()
-	#mrss_feed = XML.ElementFromURL(MRSS_PATH % id)
-
-	#for item in mrss_feed.xpath('//item'):
-	#	url = item.xpath('./link')[0].text
-	#	title = item.xpath('./title')[0].text
-	#	summary = item.xpath('./description')[0].text
-	#	date = Datetime.ParseDate(item.xpath('./pubDate')[0].text).date()
-	#	thumb = item.xpath('.//media:thumbnail', namespaces=MRSS_NS)[0].get('url')
-	#	duration = int(float(item.xpath('.//media:content', namespaces=MRSS_NS)[0].get('duration'))*1000)
 
 		oc.add(VideoClipObject(url=url, title=title, summary=summary, originally_available_at=date,
 			thumb=Resource.ContentsOfURLWithFallback(url=thumb, fallback=ICON)))
