69c524783f88b6fa493c8df2418057b8479859fd
shopgirl284 <shopgirl284@gmail.com>
2014-08-05 13:23:21 -0400

    Update code for new website
    This is a complete rewrite for the new web page design. I chose to use
    the "All Shows" json feed on the full episode page to pull shows and
    included the standup and sample feed to pick up any other full episodes

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,96 +1,93 @@
-BASE_URL = "http://www.comedycentral.com"
-MRSS_PATH = "http://www.comedycentral.com/feeds/mrss?uri=%s"
-MRSS_NS = {"media": "http://search.yahoo.com/mrss/"}
-RE_DURATION = Regex('([0-9]+)')
-SHOW_EXCLUSIONS = ["The Daily Show", "The Daily Show with Jon Stewart", "The Colbert Report", "South Park"]
+BASE_URL = "http://www.cc.com"
+SHOW_EXCLUSIONS = ["The Daily Show With Jon Stewart", "The Colbert Report", "South Park", "The Daily Show"]
 
+# This json feed pulls the list of shows with full episodes from the CC home page full episode pull down menu at the top
+# It includes Broad City, Workaholics, Review, Adam Devine House Party, and Kroll Show which are not picked up by our show feed we use
+# but it does not pull Tosh.0 or UCB which have many more episodes. Also it only gives the web page url, so you would need to pull the show id
+#NAV_FEED = 'http://www.cc.com/modules/ent_m066_cc/1.1/201ae6af-6825-4f8c-92a4-cf6c58271c88'
+# Below is the json feed url for producing all full episodes for a show where %s is the show id
+#FEED_JSON_URL = 'http://www.cc.com/feeds/f1010/1.0/5a123a71-d8b9-45d9-85d5-e85508b1b37c/%s/1'
+# This Regex will pull the json within the html text of a CC web page
+#RE_JSON = Regex("var triforceManifestFeed = (.+?);")
+
+# This json feed pulls the list of shows with full episodes from the CC full episode page "All Shows" pull down menu (about half way down the page)
+SHOW_FEED = 'http://www.cc.com/feeds/ent_m081_cc/1.0/4043f1d9-d18f-48a3-89e0-68acad5236f1'
+# The json feeds below pulls the carousel sections on the Full Episode page
+STANDUP_FEED = 'http://www.cc.com/feeds/ent_m080_cc/1.0/ee4047bd-e5aa-474c-aa62-e7415535e276'
+SAMPLE_FEED = 'http://www.cc.com/feeds/ent_m080_cc/1.0/1159cd2a-34d8-42ed-8db6-c479c5c6ba65'
 ####################################################################################################
 def Start():
 
-	ObjectContainer.title1 = "Comedy Central"
-	HTTP.CacheTime = CACHE_1HOUR
-	HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:18.0) Gecko/20100101 Firefox/18.0'
+    ObjectContainer.title1 = "Comedy Central"
+    HTTP.CacheTime = CACHE_1HOUR
+    HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:18.0) Gecko/20100101 Firefox/18.0'
 
 ####################################################################################################
 @handler("/video/comedycentral", "Comedy Central")
 def MainMenu():
 
-	oc = ObjectContainer()
-
-	for show in HTML.ElementFromURL("http://www.comedycentral.com/shows").xpath('//ul[@class="shows_list"]/li'):
-		name = show.xpath('.//meta[@itemprop="name"]')[0].get('content').encode("utf-8")
-
-		if name in SHOW_EXCLUSIONS:
-			continue
-
-		show_url = show.xpath('.//meta[@itemprop="url"]')[0].get('content').encode("utf-8")
-		summary = String.StripTags(show.xpath('.//meta[@itemprop="description"]')[0].get('content').replace('Â®', '®'))
-		thumb = show.xpath('.//img')[0].get('src').split('?')[0]
-
-		oc.add(DirectoryObject(
-			key = Callback(Episodes, show_url=show_url, title=name),
-			title = name,
-			summary = summary,
-			thumb = Resource.ContentsOfURLWithFallback(url=thumb)
-		))
-
-	return oc
+    oc = ObjectContainer()
+    oc.add(DirectoryObject(key = Callback(Shows, title="Shows"), title = "Shows"))
+    oc.add(DirectoryObject(key = Callback(VideoFeed, title="Standup Specials", show_url=STANDUP_FEED), title = "Standup Specials"))
+    oc.add(DirectoryObject(key = Callback(VideoFeed, title="Free Samples", show_url=SAMPLE_FEED), title = "Free Samples"))
+    return oc
 
 ####################################################################################################
-@route("/video/comedycentral/episodes")
-def Episodes(show_url, title):
+# This builds a directory of shows from the Full Episode page "all Shows" pull down about half way down the page
+@route("/video/comedycentral/shows")
+def Shows(title):
 
-	oc = ObjectContainer(title2=title)
+    oc = ObjectContainer(title2=title)
 
-	if not show_url.startswith('http://'):
-		show_url = ("http://www.comedycentral.com" + show_url)
+    json = JSON.ObjectFromURL(SHOW_FEED, cacheTime = CACHE_1DAY)
+    for shows in json['result']['shows']:
+        show_url = shows['fullEpisodesFeedURL']
+        show_title = shows['show']['title']
+        Log('the value of show_title is %s' %show_title)
+        if show_title in SHOW_EXCLUSIONS:
+            continue
+        show_desc = shows['show']['shortDescription'].replace('ABOUT THE SERIES: ', '')
+        show_img_hgt = 0
+        show_img_total = len(shows['show']['images'])
+        x=0
+        # There are 3 to 6 images available. This allows us to look for a larger image or at least one that is a proper icon size
+        while show_img_hgt < 400 and x<show_img_total:
+            show_img_hgt = shows['show']['images'][x]['height']
+            show_img = shows['show']['images'][x]['url']
+            x=x+1
+        oc.add(DirectoryObject(key = Callback(VideoFeed, show_url=show_url, title=show_title), title = show_title, summary = show_desc, thumb = Resource.ContentsOfURLWithFallback(url=show_img)))
+            
+    return oc
 
-	show_page = HTML.ElementFromURL(show_url)
-	episodes = show_page.xpath('//div[@class="content"]//div[@itemtype="http://schema.org/TVEpisode"]')
-	if len(episodes) > 0:
-		Log("Found %s episodes" % len(episodes))
-		pass
-	else:
-		try:
-			episodes_url = show_page.xpath('//a[@class="episodes"]')[0].get('href')
-		except:
-			episodes_url = show_url + 'full-episodes/'
-		
-		Log("Checking new page for episodes: %s" % episodes_url)
-		show_page = HTML.ElementFromURL(episodes_url)
-		episodes = show_page.xpath('//div[@class="content"]//div[@itemtype="http://schema.org/TVEpisode"]')
+####################################################################################################
+@route("/video/comedycentral/videofeed")
+def VideoFeed(title, show_url):
 
-		if len(episodes) < 1:
-			''' one last try to grab at least one episode '''
-			try:
-				HTTP.Request(show_url + 'full-episodes/', follow_redirects=False).headers
-			except Ex.RedirectError, e:
-				try:
-					episode_page = e.location
-					oc.add(URLService.MetadataObjectForURL(episode_page))
-				except:
-					pass
+    oc = ObjectContainer(title2=title)
 
-	for episode in episodes:
-		url = episode.xpath('.//meta[@itemprop="url"]')[0].get('content')
-		title = episode.xpath('.//meta[@itemprop="name"]')[0].get('content')
-		thumb = episode.xpath('.//meta[@itemprop="image"]')[0].get('content')
-		summary = episode.xpath('.//meta[@itemprop="description"]')[0].get('content')
-		date = episode.xpath('.//meta[@itemprop="datePublished"]')[0].get('content')
-		if date != "":
-			date = Datetime.ParseDate(date).date()
-		else:
-				date = None
-		
-		oc.add(VideoClipObject(
-			url = url,
-			title = title,
-			summary = summary,
-			originally_available_at = date,
-			thumb = Resource.ContentsOfURLWithFallback(url=thumb)
-		))
-
-	if len(oc) < 1:
-		return ObjectContainer(header="Error", message="This category does not contain any video.")
-	else:
-		return oc
+    json = JSON.ObjectFromURL(show_url)
+    for video in json['result']['episodes']:
+        vid_name = video['title']
+        vid_url = video['url']
+        vid_duration = Datetime.MillisecondsFromString(video['duration'])
+        vid_desc = video['description']
+        vid_date = int(video['airDate'])
+        vid_date = Datetime.FromTimestamp(vid_date)
+        vid_img = video['images'][0]['url']
+        episode = video['season']['episodeNumber']
+        season = video['season']['seasonNumber']
+        Log('the value of season is %s and episode is %s' %(season, episode))
+        oc.add(EpisodeObject(
+            url = vid_url,
+            duration = vid_duration,
+            title = vid_name,
+            summary = vid_desc,
+            originally_available_at = vid_date,
+            season = int(season),
+            index = int(episode),
+            thumb = Resource.ContentsOfURLWithFallback(url=vid_img)
+        ))
+    if len(oc) < 1:
+        return ObjectContainer(header="Error", message="This category does not contain any video.")
+    else:
+        return oc
